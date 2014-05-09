13222d4ecd34d91ae0cc318c6638279031375948
unknown <jlaw@C00823.usurf.usu.edu>
2014-05-09 16:37:03 -0600

    Added changes to the files and must keep them updated... Really I'm just re committing to make sure that I have up to date files on the git repository.

--- controllers/main.js
+++ controllers/main.js
@@ -1,8 +1,11 @@
 var myApp = angular.module("myApp", []);
+
 myApp.factory("Data", function()
 {
    return {message : "I'm data from a service"}
 })
+
+
 function firstCtrl($scope, Data)
 {
    $scope.data = Data;
